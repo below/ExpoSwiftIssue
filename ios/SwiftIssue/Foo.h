bc6ac3cda0d2f44a194dd8e610113ffe6826d18b
Alexander von Below <alex@vonbelow.com>
2023-05-13 09:58:06 +0200

    Importing the file in pure Objective-C works

---
+++ ios/SwiftIssue/Foo.h
@@ -0,0 +1,16 @@
+//
+//  Foo.h
+//  SwiftIssue
+//
+//  Created by Alexander von Below on 12.05.23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Foo : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/SwiftIssue/Foo.mm
@@ -0,0 +1,14 @@
+//
+//  Foo.m
+//  SwiftIssue
+//
+//  Created by Alexander von Below on 12.05.23.
+//
+
+#import "Foo.h"
+
+#import "SwiftIssue-Swift.h"
+
+@implementation Foo
+
+@end
